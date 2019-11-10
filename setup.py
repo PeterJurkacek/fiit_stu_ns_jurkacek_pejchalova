de2df800d36e17cc16ab7653e6e2f02037453b1a
PeterJurkacek <peter.juek@gmail.com>
2019-11-10 20:04:43 +0100

    project init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repositar projektu rieseneho v ramci predmetu neuronove siete na FIIT STU',
+    author='Peter Jurkáček',
+    license='',
+)
