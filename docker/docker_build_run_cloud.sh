22f4e2bec513af856b3b4bfc61f9cfb23184209e
PeterJurkacek <peter.juek@gmail.com>
2019-11-28 10:06:13 +0100

    docker build tensorflow gpu

--- docker/docker_build_run_cloud.sh
+++ docker/docker_build_run_cloud.sh
@@ -3,6 +3,11 @@
 image_name=pjurkacek/data-science:version1.0.0
 docker build -t $image_name .
 
+export PYTHONPATH=$PYTHONPATH:$PWD
+pip install matplotlib
+sudo docker run --rm -v /home/xjurkacekp/fiit_stu_ns_jurkacek_pejchalova:/labs --gpus all -it tensorflow/t
+ensorflow:2.0.0-gpu-py3
+
 docker run\
  -u $(id -u):$(id -g)\
  --rm\
